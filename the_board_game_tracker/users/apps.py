8fc48320cb0f74667e36268a8e78acc7568395a9
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2023-01-03 23:29:42 +0000

    cookiecutter setup

---
+++ the_board_game_tracker/users/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class UsersConfig(AppConfig):
+    name = "the_board_game_tracker.users"
+    verbose_name = _("Users")
+
+    def ready(self):
+        try:
+            import the_board_game_tracker.users.signals  # noqa F401
+        except ImportError:
+            pass
