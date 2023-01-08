bf53c308460897b439ace227a8afb2f4aa1b2880
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2023-01-08 19:36:22 +0000

    homepage updated with leaderboard

--- games/admin.py
+++ games/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+from .forms import BoardGameForm
 from .models import BoardGame
 
 
 @admin.register(BoardGame)
 class BoardGameAdmin(admin.ModelAdmin):
     list_display = ["name"]
+    form = BoardGameForm
---
+++ games/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.views.generic import ListView
+
+from .models import BoardGame
+
+User = get_user_model()
+
+
+class LeaderBoard(ListView):
+    """Shows the top players with fewest remaining games"""
+
+    model = User
+    queryset = User.leaderboard.with_counts().all()
+    template_name = "games/home.html"
+    context_object_name = "users"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["board_game_count"] = BoardGame.objects.count()
+        return context
