bf26f2088ff9dc025a1d95ee6c04741bc8f5d3a4
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2023-01-09 15:11:56 +0000

    Use checkbox widget for played_by in BoardGame form

--- games/forms.py
+++ games/forms.py
@@ -1,7 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.forms import ModelForm
+from django.forms.widgets import CheckboxSelectMultiple
 
 from .models import BoardGame
 
+User = get_user_model()
+
 
 class BoardGameForm(ModelForm):
-    model = BoardGame
+    class Meta:
+        model = BoardGame
+        fields = ["name", "played_by"]
+        widgets = {"played_by": CheckboxSelectMultiple}
