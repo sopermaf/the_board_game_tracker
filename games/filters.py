ab01164055f93f45df38dceddad1515e514e9523
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2023-01-09 18:46:32 +0000

    exclude player filter added

--- games/filters.py
+++ games/filters.py
@@ -1,9 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.forms import CheckboxSelectMultiple
 from django_filters import FilterSet
+from django_filters.filters import ModelMultipleChoiceFilter
 
-from .models import BoardGame
+from .models import BoardGame, GameCategory
+
+User = get_user_model()
 
 
 class BoardGameFilter(FilterSet):
+    # not_played_by = ModelChoiceFilter(choices=, )
+    not_played_by = ModelMultipleChoiceFilter(
+        field_name="played_by",
+        exclude=True,
+        queryset=User.objects.all(),
+        widget=CheckboxSelectMultiple,
+    )
+    category = ModelMultipleChoiceFilter(
+        field_name="category",
+        queryset=GameCategory.objects.all(),
+        widget=CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = BoardGame
-        fields = ["played_by", "category"]
+        fields = ["category", "not_played_by"]
