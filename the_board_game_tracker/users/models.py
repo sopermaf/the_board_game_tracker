c8536524279a95fe5f8c1184633ef88a7a2905c5
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2023-03-04 23:06:40 +0000

    annotations for is_hot, is_cold and is_dead

--- the_board_game_tracker/users/models.py
+++ the_board_game_tracker/users/models.py
@@ -1,12 +1,14 @@
+from datetime import date, timedelta
 from decimal import Decimal
 
 from django.contrib.auth.models import AbstractUser, UserManager
-from django.db.models import CharField, Count, F, Max, Sum
+from django.db.models import CharField, Count, Exists, F, Max, OuterRef, Sum
 from django.db.models.functions import Coalesce
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-from games.models import BoardGame
+from games.models import BoardGame, PlayedBoardGame
 
 
 class BoardGameUserManager(UserManager):
@@ -19,7 +21,19 @@ class BoardGameUserManager(UserManager):
             completed_weight=Sum(Coalesce(F("games_played__game_weight"), Decimal(0)))
         )
         qs = qs.annotate(most_recent_game=Max("playedboardgame__date_played"))
+
+        qs = qs.annotate(
+            is_hot=Exists(self._has_played_within(days_ago=14)),
+            is_cold=~Exists(self._has_played_within(days_ago=28)),
+            is_dead=~Exists(self._has_played_within(days_ago=90)),
+        )
         return qs.order_by("number_unplayed_games", "-most_recent_game", "username")
+
+    def _has_played_within(self, days_ago: int) -> date:
+        return PlayedBoardGame.objects.filter(
+            played_by=OuterRef("pk"),
+            date_played__gte=timezone.now() - timedelta(days=days_ago),
+        )
 
 
 class User(AbstractUser):
