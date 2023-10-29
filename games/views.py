b643b358301dc1224b52f6a77a5f9b0aeae11069
Ferdia Soper Mac Cafraidh <sopermaf@tcd.ie>
2023-10-29 16:56:34 +0000

    add chart to track played game progress (#65)
    Prototype for graph of games played over time

--- games/views.py
+++ games/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.views.generic import DetailView, ListView
 from django_filters.views import FilterView
 
@@ -23,6 +25,9 @@ class LeaderBoard(ListView):
         context["hot_users"] = PlayedBoardGame.objects.leaders_of_recently_played(
             days_behind_window=30
         )
+        data = PlayedBoardGame.objects.game_played_over_time_annotation()
+        context["game_progress_chart_data"] = json.dumps(data, default=str)
+        context["usernames"] = list(User.objects.values_list("username", flat=True))
         return context
 
 
